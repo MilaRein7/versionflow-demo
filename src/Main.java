de1418364d0199cc016116fed8d463bd01c56998
MilaRein7 <lyudmilavaleri@gmail.com>
2025-10-31 19:06:40 +0300

    fix bug: only letters should be allowed in the name field

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,21 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter your name: ");
-        String name = scanner.nextLine();
+        String name;
+
+        while (true) {
+            Scanner scanner = new Scanner(System.in);
+            System.out.print("Enter your name: ");
+            name = scanner.nextLine();
+
+            if (name.matches("[a-zA-Zа-яА-Я]+")) {
+                break;
+            } else {
+                System.out.println("Неподходящий формат ввода. " +
+                        "Попробуйте ещё раз");
+            }
+        }
+
         System.out.println("Hello" + " " + name);
     }
 }
